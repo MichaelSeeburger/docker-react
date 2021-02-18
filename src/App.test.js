c0dcf5a7447f92824c735ae0d44102bff414f38f
MichaelSeeburger <39369290+MichaelSeeburger@users.noreply.github.com>
2021-02-18 11:27:38 +0100

    initial

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+it('renders welcome message', () => {
+  render(<App />);
+  expect(screen.getByText('Learn React')).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
